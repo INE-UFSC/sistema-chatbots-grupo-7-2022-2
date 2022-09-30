19e3ed438d55e3323e2d4b292ab854a90d729d48
Geovane Schmitz <contato@geovanems.com.br>
2022-09-30 10:37:51 -0300

    Registra o bot feliz no sistema

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotTriste import BotTriste
+from Bots.BotFeliz import BotFeliz
 
 # construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotTriste("Dengoso"), ]
+lista_bots = [BotZangado("Yoda"), BotTriste("Dengoso"), BotFeliz("Joy")]
 
 sys = scb.SistemaChatBot("CrazyBots", lista_bots)
 sys.inicio()

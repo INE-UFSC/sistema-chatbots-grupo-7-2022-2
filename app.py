9ec4dd945d01f50f01d4714871baaa7ad8673a4a
Geovane Schmitz <contato@geovanems.com.br>
2022-09-30 10:28:08 -0300

    Registra o bot triste no sistema

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotTriste import BotTriste
 
-###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+# construa a lista de bots disponíveis aqui
+lista_bots = [BotZangado("Yoda"), BotTriste("Dengoso"), ]
 
-sys = scb.SistemaChatBot("CrazyBots",lista_bots)
+sys = scb.SistemaChatBot("CrazyBots", lista_bots)
 sys.inicio()
